d36defa965410b5e7f4ded087c9d93106aee6b23
Jon Jaques <jon@3five.com>
2016-08-02 16:19:03 -0500

    Export redux util fns

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-export { connect }                       from 'react-redux'
-export { asyncConnect }                  from 'redux-connect'
 export { default as Helmet }             from 'react-helmet'
 export { default as cookie }             from 'react-cookie'
 export { default as Application }        from './application'
+export { asyncConnect }                  from 'redux-connect'
+
 export {
   createAction,
   handleAction,
   handleActions
 } from 'redux-actions'
+
+export { 
+  applyMiddleware,
+  bindActionCreators,
+  compose,
+  combineReducers,
+  connect, 
+  createStore,
+} from 'react-redux'
